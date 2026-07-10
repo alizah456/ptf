4175a6da9463be18793330904c3325691d490428
Abshrimali <abbi.shrimali@gmail.com>
2026-07-10 10:53:24 +0500

    my first comit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,29 +1,32 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import { lazy, Suspense } from "react";
+
+// Lazy Imports
+const Navbar = lazy(() => import("./components/Navbar"));
+const Hero = lazy(() => import("./components/Hero"));
+const About = lazy(() => import("./components/About"));
+const Skills = lazy(() => import("./components/Skills"));
+const Projects = lazy(() => import("./components/Projects"));
+const Contact = lazy(() => import("./components/Contact"));
+const Footer = lazy(() => import("./components/Footer"));
 
 function App() {
   return (
     <div className="bg-slate-950 text-white overflow-x-hidden">
-
-      <Navbar />
-
-      <Hero />
-
-      <About />
-
-      <Skills />
-
-      <Projects />
-
-      <Contact />
-
-      <Footer />
-
+      <Suspense
+        fallback={
+          <div className="flex items-center justify-center min-h-screen text-xl">
+            Loading...
+          </div>
+        }
+      >
+        <Navbar />
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+        <Footer />
+      </Suspense>
     </div>
   );
 }
